60909dbfe934eae34f55df968ee73d4461e6daaa
Mohamed El omary <mel-omar@e1r4p12.1337.ma>
2021-03-05 18:53:43 +0100

    create pipe function and convert parsing struct to execution struct

---
+++ EXECUTION/srcs/from_parsing2execution.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   from_parsing2execution.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-omar <mel-omar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 12:32:14 by mel-omar          #+#    #+#             */
+/*   Updated: 2021/03/05 17:26:36 by mel-omar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/execution.h"
+
+void        **convert_list2array2d(const t_clist *lst, size_t  size)
+{
+    void    **array_2d;
+    size_t  iterator;
+
+    array_2d = malloc(size * length(lst));
+    iterator = 0;
+    while (lst)
+    {
+        ft_memcpy(*(array_2d + iterator * size), lst->data, size);
+        lst = lst->next;
+        iterator++;
+    }
+    array_2d[iterator * size] = NULL;
+    return (array_2d);
+}
+
+static t_file   *create_file(const char *filename, const char *redirect)
+{
+    t_file      *file;
+
+    file = malloc(sizeof(t_file *));
+    file->filename = ft_strdup(filename);
+    if (!ft_strncmp(redirect, ">", 2))
+        file->redirect = OUTPUT;
+    else if (!ft_strncmp(redirect, "<", 2))
+        file->redirect = INPUT;
+    else
+        file->redirect = APPEND;
+    return (file);
+}
+
+t_clist      *combine_files_and_redirections(const t_clist *files, const t_clist *redirections)
+{
+    t_clist     *combine;
+
+    combine = NULL;
+    while (files)
+    {
+        append(&combine, create_file(files->data, redirections->data));
+        files = files->next;
+        redirections = redirections->next;
+    }
+    return (combine);
+}
+
+static t_exec  *parse_exec(const t_ccommand *command)
+{
+    t_exec      *exec;
+    t_rec       *rec;
+
+    exec = malloc(sizeof(t_exec));
+    exec->cmd = ft_strdup(command->cmd);
+    exec->arguments = convert_list2array2d(command->data->text, sizeof(char *));
+    exec->files = combine_files_and_redirections(command->data->files, command->data->oper);    
+    return (exec);
+}
+
+t_clist        *from_parsing2exec(const t_clist *lst)
+{
+    t_clist     *execution;
+
+    execution = NULL;
+    while (lst)
+    {
+        append(&execution, parse_exec(lst->data));
+        lst = lst->next;   
+    }
+    return (execution);
+}
---
+++ EXECUTION/srcs/pipe.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-omar <mel-omar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 12:25:06 by mel-omar          #+#    #+#             */
+/*   Updated: 2021/03/05 17:58:01 by mel-omar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/execution.h"
+
+void    ft_pipe(t_clist  *pipe_exec, bool is_first, int old_stdin)
+{
+    int     pid;
+    int     status;
+    int     fd[2];
+    t_exec  *exec;
+
+    if (pipe_exec)
+        return ;
+    if (pipe_exec->next)
+        pipe(fd);
+    if ((pid=fork()) == 0)
+    {
+        exec = (t_exec *)pipe_exec->data;
+        dup2(old_stdin, 0);
+        if (pipe_exec->next)
+            dup2(fd[1], 1);
+        close(fd[1]);
+        if (!is_first)
+            close(old_stdin);
+        close(fd[0]);
+        execve(exec->cmd, exec->arguments, NULL);
+        exit(127);
+    }
+    close(fd[1]);
+    if (!is_first)
+        close(old_stdin);
+    mypipe(pipe_exec->next, 0, fd[0]);
+    close(fd[0]);
+    wait(NULL);
+}
+
+int     main(void)
+{
+}
