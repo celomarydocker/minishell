a0b3ccbda7916b78a99ce25cee273caf88bf8a89
celomary <elomarymohamed72@gmail.com>
2021-03-09 12:51:41 +0100

    FiX CONFLIT ON A REPO

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-gcc PARSER/print/*.c PARSER/gnl/*.c PARSER/dt/hash_table/*.c PARSER/dt/linkedlist/*.c PARSER/command_recognizer/*.c  main.c EXECUTION/srcs/check_existance.c  PARSER/split/*.c EXECUTION/srcs/ft_echo.c libft/*.c EXECUTION/srcs/from_parsing2execution.c \
+gcc -fsanitize=adress PARSER/print/*.c PARSER/gnl/*.c PARSER/dt/hash_table/*.c PARSER/dt/linkedlist/*.c PARSER/command_recognizer/*.c  main.c EXECUTION/srcs/check_existance.c  PARSER/split/*.c EXECUTION/srcs/ft_echo.c libft/*.c EXECUTION/srcs/from_parsing2execution.c \
 EXECUTION/srcs/files.c EXECUTION/srcs/ft_pipe.c EXECUTION/srcs/pipe_helper.c EXECUTION/srcs/ft_setup_builtins.c EXECUTION/srcs/ft_pwd.c EXECUTION/srcs/free_data.c
 ./a.out
