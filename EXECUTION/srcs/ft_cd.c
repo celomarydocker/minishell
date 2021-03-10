1bdfc35c34b9f51d57dd8cc8c12de343d6602f57
Hicham Fadyl <hfadyl@student.1337.ma>
2021-03-10 19:42:10 +0100

    cd work fine

--- EXECUTION/srcs/ft_cd.c
+++ EXECUTION/srcs/ft_cd.c
@@ -23,24 +23,25 @@ void    ft_exec_cd(char **str, int fd, t_cmap *envs)
     if (!*str)
     {
         to_der = get(envs, "HOME");
-        print("old pwd %s\n", get(envs, "PWD"));
+        // print("old pwd %s\n", get(envs, "PWD"));
         chdir(to_der);
         getcwd(buffer, 64);
         setv(envs, "PWD", buffer);
-        print("new pwd %s\n", get(envs, "PWD"));
+        // print("new pwd %s\n", get(envs, "PWD"));
     }
     else
     {
         if (!(chdir(*str)))
         {
-            print("old pwd %s\n", get(envs, "PWD"));
+            // print("old pwd %s\n", get(envs, "PWD"));
             getcwd(buffer, 64);
             setv(envs, "PWD", buffer);
-            print("new pwd %s\n", get(envs, "PWD"));
+            // print("new pwd %s\n", get(envs, "PWD"));
         }
         else
         {
             ft_putstr_fd("cd : no such file or directory ", fd);
+            write(1, "\n", 1);
         }
     }
 }
