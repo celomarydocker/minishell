59446806ae4270de935e9dee24038022a1408a0b
Hicham Fadyl <hfadyl@student.1337.ma>
2021-03-11 19:31:22 +0100

    echo&cd work fine

--- main.c
+++ main.c
@@ -131,7 +131,7 @@ int     main(void)
     insert_builtins(g_builtins, "echo", ft_exec_echo);
     insert_builtins(g_builtins, "pwd", ft_exec_pwd);
     insert_builtins(g_builtins, "cd", ft_exec_cd);
-    //insert_builtins(g_builtins, "cd", ft_exec_cd);
+    //insert_builtins(g_builtins, "unset", ft_exec_cd);
     /*** END TEST ***/
 	while (1)
 	{
